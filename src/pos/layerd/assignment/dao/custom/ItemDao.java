ac5dec4be7e9b60d1ddfb85be9ea44b878fc432e
Harsha-Pradeep377 <harshapradeep9@gmail.com>
2023-09-22 01:21:01 +0530

    ItemDaoImpl, ItemServiceImpl and ItemController completed

---
+++ src/pos/layerd/assignment/dao/custom/ItemDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pos.layerd.assignment.dao.custom;
+
+import pos.layerd.assignment.dao.CrudDao;
+import pos.layerd.assignment.entity.ItemEntity;
+
+/**
+ *
+ * @author Harsha
+ */
+public interface ItemDao extends CrudDao<ItemEntity, String>{
+    
+}
---
+++ src/pos/layerd/assignment/service/custom/impl/ItemServiceImpl.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos.layerd.assignment.service.custom.impl;
+
+import java.util.ArrayList;
+import pos.layerd.assignment.dao.DaoFactory;
+import pos.layerd.assignment.dao.custom.ItemDao;
+import pos.layerd.assignment.dto.ItemDto;
+import pos.layerd.assignment.entity.ItemEntity;
+import pos.layerd.assignment.service.custom.ItemService;
+
+/**
+ *
+ * @author Harsha
+ */
+public class ItemServiceImpl implements ItemService{
+    
+    ItemDao itemDao = (ItemDao) DaoFactory.getInstance().getDao(DaoFactory.DaoType.ITEM);
+
+    @Override
+    public String addItem(ItemDto dto) throws Exception {
+        if (itemDao.add(new ItemEntity(dto.getId(), dto.getDescription(),
+                dto.getPackSize(), dto.getUnitPrice(), dto.getQoh()))) {
+            return "Successfully Saved";
+        } else {
+            return "Fail";
+        }
+    }
+
+    @Override
+    public String updateItem(ItemDto dto) throws Exception {
+         if (itemDao.update(new ItemEntity(dto.getId(), dto.getDescription(),
+                dto.getPackSize(), dto.getUnitPrice(), dto.getQoh()))) {
+            return "Successfully Update";
+        } else {
+            return "Fail";
+        }
+    }
+
+    @Override
+    public String deleteItem(String id) throws Exception {
+        if(itemDao.delete(id)){
+            return "Successfully Delete";
+        }else{
+            return "Fail";
+        }
+    }
+
+    @Override
+    public ItemDto getItem(String id) throws Exception {
+         ItemEntity entity = itemDao.get(id);
+        return new ItemDto(entity.getId(), entity.getDescription(),
+                entity.getPackSize(), entity.getUnitPrice(), entity.getQoh());
+    }
+
+    @Override
+    public ArrayList<ItemDto> getAllItem() throws Exception {
+        ArrayList<ItemDto> itemDtos = new ArrayList<>();
+        
+        ArrayList<ItemEntity> itemEntitys = itemDao.getAll();
+        
+        for (ItemEntity entity : itemEntitys) {
+            ItemDto itemDto = new ItemDto(entity.getId(), entity.getDescription(),
+                entity.getPackSize(), entity.getUnitPrice(), entity.getQoh());
+            itemDtos.add(itemDto);
+        }
+        
+        return itemDtos;
+    }
+    
+}
